9c08e26f69d28520cf6f63c4d5f98766ff4429cf
JCinelli <37580071+JCinelli@users.noreply.github.com>
2021-01-07 10:50:55 +0100

    Init

---
+++ src/main/java/com/weathair/WeathAirApplication.java
@@ -0,0 +1,13 @@
+package com.weathair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathAirApplication.class, args);
+	}
+
+}
