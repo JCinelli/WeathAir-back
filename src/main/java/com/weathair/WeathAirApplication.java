1ef8fbde9fbbb43e85f35a8e3aee940b16b83b5e
JCinelli <37580071+JCinelli@users.noreply.github.com>
2021-01-13 14:33:06 +0100

    test2

--- src/main/java/com/weathair/WeathAirApplication.java
+++ src/main/java/com/weathair/WeathAirApplication.java
@@ -2,26 +2,15 @@ package com.weathair;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 import javax.annotation.PostConstruct;
 
-import org.hibernate.type.LocalDateType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.weathair.controllers.MeteoIndicatorController;
 import com.weathair.controllers.TownshipController;
-import com.weathair.dto.indicators.MeteoIndicatorDTO;
-import com.weathair.entities.indicators.MeteoIndicator;
-import com.weathair.repositories.TownshipRepository;
 import com.weathair.services.MeteoIndicatorService;
 
 @SpringBootApplication
